ea05da6d2ac1e399b15da076fafa5862bf0e772d
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-09-25 03:29:02 +0530

    extends from Indecision App

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Indecision from "./components/Indecision"
 import "./styles/style.scss"
 import "normalize.css/normalize.css"
 
-ReactDOM.render(<Indecision />, document.getElementById('app'));
+ReactDOM.render(<p>This is a boilerplate</p>, document.getElementById('app'));
