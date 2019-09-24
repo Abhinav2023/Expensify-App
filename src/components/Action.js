02572bb662d4628879ad79ef49c4389f3ad781b3
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-09-25 03:16:13 +0530

    Nothing to say

--- src/components/Action.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Action=(props)=>(
-    <div>
-        <button className="big-button" disabled={!props.hasOption} onClick={props.RandomPick}>What Should I do?</button>
-    </div>
-)
-export default Action;
