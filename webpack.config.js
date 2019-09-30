134aeadfcf3b89988122bc14f3b44d058c838ce3
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-09-30 21:31:19 +0530

    organising redux

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 var webpack =require("webpack");
 
 module.exports = {
-  entry: './src/playground/Redux-Expensify.js',
+  entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
