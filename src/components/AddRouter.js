04c70264084a6bf4e3150242a86facf5a8ce3f14
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-10-02 20:10:01 +0530

    Added addExpense functionality

--- src/components/AddRouter.js
+++ src/components/AddRouter.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addExpense} from '../actions/expenses'
 import ExpenseForm from './ExpenseForm';
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
     <div>
-      <ExpenseForm />
+      <ExpenseForm 
+        onSubmit={(expense)=>{
+          props.dispatch(addExpense(expense));
+          props.history.push("/");
+        }}
+      />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
