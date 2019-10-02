edd03108bc0f89c7ce1721a645ec54c4600b8e0b
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-10-02 15:53:29 +0530

    Added Remove button

--- src/app.js
+++ src/app.js
@@ -13,15 +13,10 @@ const store=configureStore();
 
 store.dispatch(addExpense({description: 'Water Bill'}));
 store.dispatch(addExpense({description: 'Gas Bill'}));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(()=>{
-    store.dispatch(setTextFilter('bill'));
-},3000);
+store.dispatch(setTextFilter('bill'));
 
 const state=store.getState();
 const visibleExpenses=getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx=(
     <Provider store={store}>
