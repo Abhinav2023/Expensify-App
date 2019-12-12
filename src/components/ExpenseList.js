3a2fcd0c70918cb0f06d89758d6e44d8d74fd0f1
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-12-12 12:18:51 +0530

    add expense summay to dashboard

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,13 +3,18 @@ import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import SelectExpenses from '../selectors/expenses';
 
+
 export const ExpenseList=(props)=>(
+    
     <div>
         <h1>Expenses List</h1>
+
         {
             props.expenses.length===0 ? (
                 <p>No expenses</p>
             ) : (
+
+                
                 props.expenses.map((expense)=>{
                     return <ExpenseListItem key={expense.id} {...expense}/>
                 })
