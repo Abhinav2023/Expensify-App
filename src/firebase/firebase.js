5c10af4878744e80566a0141843cc11593919a39
Abhinav2023 <abhinavbansal231101@gmail.com>
2019-12-18 11:03:45 +0530

    Firebase basics learned

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,32 +13,127 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
  // firebase.analytics();
+const database=firebase.database();
 
- const database=firebase.database();
+// database.ref('expenses').push({
+//     id: '1',
+//     description: "gum",
+//     note: '',
+//     amount: 195,
+//     createdAt: 0
+// });
 
-database.ref().set({
-    name: 'Abhinav Bansal',
-    age: 18,
-    location:{
-        city: 'Sawai Madhopur',
-        country: 'India'
-    }
-}).then(()=>{
-    console.log('data is saved');
-}).catch((error)=>{
-    console.log('This failed', error);
-})
-database.ref('age').set(27);
-database.ref('location/city').set('Allahabad');
-
-database.ref('attributes').set({
-    height: 24,
-    weight: 45
-}).then(()=>{
-    console.log('data is saved perfectly!');
-}).catch((e)=>{
-    console.log('Error expected', e);
+database.ref('expenses').on('child_removed',(snapshot)=>{
+    console.log(snapshot.key, snapshot.val());
 })
 
-console.log('I made a request to change to database');
+database.ref('expenses').on('child_changed',(snapshot)=>{
+    console.log(snapshot.key, snapshot.val());
+});
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot)=>{
+//         const expenses=[];
+//         snapshot.forEach((childSnapshot)=>{
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             })
+//         })
+//         console.log(expenses);
+//     });
 
+// database.ref('expenses')
+//     .on('value',(snapshot)=>{
+//         const expenses=[];
+//         snapshot.forEach((childSnapshot)=>{
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             })
+//         })
+//         console.log(expenses);
+//     });
+
+// setTimeout(()=>{
+//     database.ref('expenses').push({
+//         id: '1',
+//         description: "Rent",
+//         note: '',
+//         amount: 19500,
+//         createdAt: 0
+//     });
+// },4000);
+
+
+
+
+// const firebaseNotes={
+//     notes: {
+//         nnjnonl: {
+//             title: 'First note',
+//             des: 'This is my first description of note'
+//         },
+//         nwoniojwoi: {
+//             title: 'Second note',
+//             des: 'This is my first description of note'
+//         }
+//     }
+// };
+
+// database.ref().set(firebaseNotes);
+
+// database.ref('notes').set(
+//     [
+//         {
+//             title: 'First note',
+//             des: 'This is my first description of note'
+//         },
+//         {
+//             title: 'Second note',
+//             des: 'This is my first description of note'
+//         }
+//     ]
+// );
+
+// database.ref().on('value',(snapshot)=>{
+//     console.log(snapshot.val());
+// });
+
+// setTimeout(()=>{
+//     database.ref().update({
+//         Stresslevel: 0,
+//         'job/company': 'Microsoft'
+//     })
+// },4000);
+ 
+
+
+// database.ref().set({
+//     name: 'Abhinav Bansal',
+//     age: 18,
+//     Stresslevel: 6,
+//     job: {
+//         title: 'software developer',
+//         company: 'Google'
+//     },
+//     location:{
+//         city: 'Sawai Madhopur',
+//         country: 'India'
+//     }
+// }).then(()=>{
+//     console.log('data is saved');
+// }).catch((error)=>{
+//     console.log('This failed', error);
+// });
+
+// database.ref().update({
+//     name: 'Abhinav',
+//     'location/city': 'Banglore',
+//     'job/company': 'Amazon',
+//     Stresslevel: 8
+// }).then(()=>{
+//     console.log('Database updated')
+// }).catch((e)=>{
+//     console.log('error'+ e);
+// });
